455ffe2c94c850de3e92b558d2204000ab488ab6
Fnhid <fnhid05@gmail.com>
2021-11-07 22:19:49 +0900

    signin&up page init

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ app = Flask(__name__)
 # 0.0.0.0:5000
 @app.route("/")
 def main():
-    return render_template('main.html')
+    return render_template('main_ailen.html')
 
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
 
+@app.route("/signin")
+def signin():
+    return render_template('signin.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
