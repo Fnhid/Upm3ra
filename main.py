5dc3af12b6419559ee8f744a9d907b16d6a00914
Fnhid <fnhid05@gmail.com>
2021-11-29 20:18:25 +0900

    v1.0-alpha1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, redirect, request, url_for
+from flask import Flask, render_template, jsonify, redirect, request, url_for, session
 from functools import wraps
 import pymysql
 import time
